8d31705f54baa4bea7b6957d1c458534abc40c7b
doublik <doublik99@gmail.com>
2018-11-26 15:20:27 +0900

    add timer and some animations

--- Hangman/PracticeModeController.java
+++ Hangman/PracticeModeController.java
@@ -3,48 +3,91 @@ package Hangman;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.scene.paint.Paint;
 import javafx.util.Duration;
 
-import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class PracticeModeController implements Initializable {
 
+    @FXML private AnchorPane GamePane;
+    @FXML private BorderPane CountDownPane;
+    @FXML private Label txtCount, txtDelete;
+    @FXML private ProgressBar timeBar;
+    @FXML private ImageView human;
+    @FXML private Label rope;
+
     private Timeline timeline;
     private final int tick = 3;
-    private int lefttick;
-
-    @FXML private BorderPane background;
-    @FXML private Label txtCount;
+    private final int GameTime = 60000; // ms (60000 = 1 min)
+    private int leftTick, leftTime;
+    private final String[] words = new String[] {"APPLE","STUDENT","CHARACTER"};
+    private char[] answer;
+    private char[] uranswer;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        CountDownPane.setVisible(true);
+        timeBar.setStyle("-fx-accent: #ffae00");
         startCountDown();
+        // set word
+        Random random = new Random();
+        String temp = words[random.nextInt(words.length)]; //words[(int)Math.round(Math.random()*(words.length-1))];
+        answer = new char[temp.length()];
+        uranswer = new char[temp.length()];
+        for (int i=0; i<temp.length(); i++) {
+            answer[i] = temp.charAt(i);
+            uranswer[i] = '.';
+            System.out.println(answer[i] + "" + uranswer[i]);
+        }
     }
 
     private void startGame() {
-        background.setStyle("-fx-background-color: WHITE");
+        CountDownPane.setVisible(false);
+
+        leftTime = GameTime;
+        timeline = new Timeline(new KeyFrame(Duration.millis(10), event -> {
+            leftTime -= 10;
+            if (leftTime <= 0) {
+                timeline.stop();
+            }
+            timeBar.setProgress((double)leftTime/60000);
+            downRope(0.081);
+            if ((human.getLayoutY()+332 >= 570) && (leftTime%100 == 0)) { //((leftTime < 40000) && (leftTime%100 == 0)) {
+                if (txtDelete.getTextFill() == Paint.valueOf("ORANGE")) {
+                    txtDelete.setTextFill(Paint.valueOf("WHITE"));
+                } else {
+                    txtDelete.setTextFill(Paint.valueOf("ORANGE"));
+                }
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void downRope(double value) {
+        rope.setPrefHeight(rope.getPrefHeight() + value);
+        human.setLayoutY(human.getLayoutY() + value);
     }
 
     private void startCountDown() {
-        lefttick = tick;
-        txtCount.setText("" + lefttick);
+        leftTick = tick;
+        txtCount.setText("" + leftTick);
         timeline = new Timeline(new KeyFrame(Duration.seconds(1),event -> {
-            lefttick--;
-            if (lefttick <= 0) {
+            leftTick--;
+            if (leftTick <= 0) {
                 timeline.stop();
-                txtCount.setVisible(false);
                 startGame();
             } else { // if lefttick is 0, Text don't change.
-                txtCount.setText("" + lefttick);
+                txtCount.setText("" + leftTick);
             }
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
